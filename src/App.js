ec73408417de2d4f474959487c75529827713b19
Ghita ZIOUI <ghita.zioui@gmail.com>
2020-09-09 17:25:16 +0200

    Created Header and LandingPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LandingPage from './LandingPage.js'
+
 
 function App() {
   return (
     <div>
-        <p> Mon projet porfolio </p>
+        <LandingPage />
     </div>
   );
 }
---
+++ src/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header.js'
+
+const LandingPage = function(){
+     return <div>
+         <Header title = "Ghita ZIOUI." />
+         
+     </div>
+
+}
+
+export default LandingPage
